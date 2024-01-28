3454463d83c99d93d8d57e6087da41215d9a1980
FrankLi123 <zoli@ucsd.edu>
2024-01-28 11:00:47 -0500

    create function 'getDeployedCampaigns'

---
+++ contract/factory.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    /* factory help a manager creates a new campaign */
+    function createCampaign(uint minimumVal) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    /* get the list of all created campaigns */
+    function getDeployedCampaigns() public view returns (address[]){
+        return deployedCampaigns;
+    }
+
+}
