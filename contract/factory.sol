e839c26516bdb97f00f2aa8728a508c926b53cc8
FrankLi123 <zoli@ucsd.edu>
2024-01-28 11:09:58 -0500

    small fix

--- contract/factory.sol
+++ contract/factory.sol
@@ -1,11 +1,12 @@
 pragma solidity ^0.4.17;
+import "./ballot.sol";
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
     /* factory help a manager creates a new campaign */
     function createCampaign(uint minimumVal) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
+        address newCampaign = new Campaign(minimumVal, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
 
