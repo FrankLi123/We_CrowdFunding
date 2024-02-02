8ae65bb391c78827b723796296a41666d87ffb7a
FrankLi123 <zoli@ucsd.edu>
2024-02-02 10:36:13 -0500

    move the link compoennt to Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,9 +7,12 @@ export default (props)=>{
     return (
 
         <Container>
-
+          <link
+            async
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
         <div>
-
 
             <Header/>
 
--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,6 @@ export default function Home({ campaigns }) {
 
         <Layout>
       <div>
-          <link
-            async
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
         <h3> Open Campaigns</h3>
        
         <Button floated="right" content="Create Campaign" icon="add circle" primary/>
