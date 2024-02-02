8e9fbb1f277e6df2fddf4c8d7ea006a7be9026e1
FrankLi123 <zoli@ucsd.edu>
2024-02-02 10:41:44 -0500

    add Header to new.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Header from './Header';
 import {Container} from 'semantic-ui-react';
+import Head from "next/head";
 
 export default (props)=>{
 
     return (
 
         <Container>
+            
+            {/* <Head> - automatically moved the the head tag of HTML documents */}
+        <Head> 
           <link
             async
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
         />
+        </Head>
+        
         <div>
 
             <Header/>
--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,9 +1,15 @@
 import React,{Component} from "react";
+import Layout from "../../components/Layout";
 
 class CampaignNew extends Component {
 
-    render (){
-        return <h1> !!!!</h1>
+    render(){
+        return (
+            <Layout> 
+            <h1> !!!!</h1>
+            </Layout>
+        )
+
     }
 }
 
