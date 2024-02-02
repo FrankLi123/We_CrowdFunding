c0b020e4500dbb980d6b7535c9bd12d7c91e9d2d
FrankLi123 <zoli@ucsd.edu>
2024-02-02 10:43:51 -0500

    new form for input minimum contribution

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,15 +1,20 @@
 import React,{Component} from "react";
 import Layout from "../../components/Layout";
+import { Card, Button, Form} from 'semantic-ui-react';
 
 class CampaignNew extends Component {
 
     render(){
         return (
-            <Layout> 
-            <h1> !!!!</h1>
+            <Layout>
+                <h3> Create a New Campaign</h3>
+                <Form.Field>
+                    <label> Minimum Contribution</label>
+                    <input/>
+                </Form.Field>
+                <Button primary> Create: </Button>
             </Layout>
         )
-
     }
 }
 
