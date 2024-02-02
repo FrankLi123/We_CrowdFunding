bd6cc1e6d4376008748987ea37104080bb049cc1
FrankLi123 <zoli@ucsd.edu>
2024-02-02 10:55:18 -0500

    add state, and event handler on Form

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -4,13 +4,19 @@ import { Card, Button, Form, Input} from 'semantic-ui-react';
 
 class CampaignNew extends Component {
 
+    state = {
+        minimumContribution: ''
+    };
     render(){
         return (
             <Layout>
                 <h3> Create a New Campaign</h3>
                 <Form.Field>
                     <label> Minimum Contribution</label>
-                    <Input label="wei" labelPosition="right"/>
+                    <Input label="wei" labelPosition="right"
+                    value={this.state.minimumContribution}
+                    onChange={event => this.setState({minimumContribution: event.target.value})}
+                    />
                 </Form.Field>
                 <Button primary> Create: </Button>
             </Layout>
