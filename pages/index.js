b2bb3b7e526243a30d0113e0fc9ae88ea917cac1
FrankLi123 <zoli@ucsd.edu>
2024-02-03 23:28:36 -0500

    route to create campaign page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button} from 'semantic-ui-react';
 import Layout from '../components/Layout';
-
+import {Link} from '../routes';
 
 export default function Home({ campaigns }) {
     const items = campaigns.map((item) => {
@@ -19,7 +19,12 @@ export default function Home({ campaigns }) {
       <div>
         <h3> Open Campaigns</h3>
        
-        <Button floated="right" content="Create Campaign" icon="add circle" primary/>
+        <Link route="/campaigns/new">
+            {/* enable traditional right click functionality  */}
+            <a>
+                <Button floated="right" content="Create Campaign" icon="add circle" primary/>
+            </a>
+        </Link>
         <Card.Group items={items} />
       </div>
       </Layout>
