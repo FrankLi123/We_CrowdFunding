0cc26f2fdc6adc4b60855d187cc8a70870bbf49f
FrankLi123 <zoli@ucsd.edu>
2024-02-03 23:37:54 -0500

    fix the bug in route mapping (avoid default routing for /campaigns/new

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 const routes = require("next-routes")();
 
 //define a new route mapping with wildcard ':'
-routes.add('/campaigns/:address', '/campaigns/show' );
+routes.add('/campaigns/new', '/campaigns/new')
+.add('/campaigns/:address', '/campaigns/show' );
 
 module.exports = routes;
