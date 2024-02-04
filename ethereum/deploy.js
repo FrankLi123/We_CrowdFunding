bfb0681f11a1d35e43fbb5b45f21eee928143d18
FrankLi123 <zoli@ucsd.edu>
2024-02-03 23:52:50 -0500

    clear redundant comments

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -7,10 +7,7 @@ console.log(mnemonic);
 
 const provider = new HDWalletProvider(
     mnemonic,
-  // remember to change this to your own phrase!
     'https://sepolia.infura.io/v3/0bc26115df1c4fb98cd9aa4081885505'
-//   'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
 const deploy = async () => {
