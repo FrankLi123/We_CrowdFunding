558bdb4b704809a751c140d9cb137d1695d65d47
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:31:43 -0500

    add a new routing path to '/requests/new'

--- routes.js
+++ routes.js
@@ -5,6 +5,8 @@ const routes = require("next-routes")();
 //define a new route mapping with wildcard ':'
 routes.add('/campaigns/new', '/campaigns/new')
 .add('/campaigns/:address', '/campaigns/show' )
-.add('/campaigns/:address/requests', '/campaigns/requests/index');
+.add('/campaigns/:address/requests', '/campaigns/requests/index')
+.add('/campaigns/:address/requests/new', '/campaigns/requests/new');
+
 
 module.exports = routes;
