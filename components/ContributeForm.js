4d71208dc01b63d8353d1cce8bebfa382bfe8848
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:09:07 -0500

    use Router.replaceRoute to refresh the page when the transaction completes

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, Button, Form, Input, Message} from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from 'next/router';
 
 class ContributeForm extends Component {
     
@@ -24,6 +25,10 @@ class ContributeForm extends Component {
                 from: accounts[0],
                 value: web3.utils.toWei(this.state.value, 'ether')
             });
+
+            // refresh the page and see the change
+            Router.replaceRoute(`/campaigns/${this.props.address}`)
+
         }catch(err){
             // this.setState({errorMessage: err.message});
         }
