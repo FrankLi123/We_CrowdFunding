8b271e589b64da90a5b986a9abb4ba4e0e4bf53e
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:42:26 -0500

    add Forms and event handlers to the class 'RequestNew'

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
-
-
+import { Card, Button, Form, Input, Message, Grid} from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import Layout from '../../../components/Layout';
 class RequestNew extends Component {
 
+
+    state = {
+        description: '',
+        value: '',
+        recipient: ''
+    };
+
+    static async getInitialProps(props){
+        const {address} = props.query;
+        
+        return {address};
+    }
+
     render() {
-        return <h3> Create a Request</h3>
+        return (
+
+            <Layout>
+            <Form>
+                <Form.Field>
+                    <label> Description</label>
+                    <Input 
+                    value={this.state.description}
+                    onChange ={event =>
+                    this.setState({description: event.target.value})}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label> Value (Ether) </label>
+                    <Input 
+                    value={this.state.value}
+                    onChange ={event =>
+                    this.setState({value: event.target.value})}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label> Recipient </label>
+                    <Input 
+                    value={this.state.recipient}
+                    onChange ={event =>
+                    this.setState({recipient: event.target.value})}
+                    />
+                </Form.Field>
+                <Button primary> Create !</Button>
+            </Form>
+            </Layout>
+        )
     }
 }
 
