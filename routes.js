1cc01caf011bc7470c08b2a43e305bc5b3f8a5a9
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:20:02 -0500

    add a routing '/campaigns/:address/requests' to '/campaigns/requests/index'

--- routes.js
+++ routes.js
@@ -4,6 +4,7 @@ const routes = require("next-routes")();
 
 //define a new route mapping with wildcard ':'
 routes.add('/campaigns/new', '/campaigns/new')
-.add('/campaigns/:address', '/campaigns/show' );
+.add('/campaigns/:address', '/campaigns/show' )
+.add('/campaigns/:address/requests', '/campaigns/requests/index');
 
 module.exports = routes;
