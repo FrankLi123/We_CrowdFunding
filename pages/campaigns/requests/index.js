6bd8cdd533b01777ec950e35eb90f74535c3f35a
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:30:09 -0500

    create a new file 'new.js' for creating new request and a button routing to this page.

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,12 +1,21 @@
 // show list of a request
 
 import React, { Component } from 'react';
-
+import Layout from '../../../components/Layout';
+import {Link} from '../../../routes';
+import { Card, Button} from 'semantic-ui-react';
 
 class RequestIndex extends Component {
     render(){
         return(
-            <h3> Request List</h3>
+            <Layout>
+                <h3> Request List</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                <a>
+                    <Button primary> Add Request</Button>
+                </a>
+                </Link>
+            </Layout>
         )
     };
 };
