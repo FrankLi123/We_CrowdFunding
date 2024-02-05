731515b88964aac7e2bbff23d11258700d0dcd88
FrankLi123 <zoli@ucsd.edu>
2024-02-05 01:04:51 -0500

    impl function 'onSubmit' for making one contribution for current campaign

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,16 +1,47 @@
 import React, {Component} from 'react';
 import { Card, Button, Form, Input, Message} from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
 class ContributeForm extends Component {
+    
+    state = {
+        value: ''
+    };
+    
+
+    // trigger the creation of new campaign
+    onSubmit = async event => {
+        event.preventDefault();
+        
+        // this.setState({loading: true, errorMessage: ''});
+        const campaign = Campaign(this.props.address);
+
+        try{
+            const accounts = await web3.eth.getAccounts();
+
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+        }catch(err){
+            // this.setState({errorMessage: err.message});
+        }
+
+        // this.setState({loading: false});
+    };
+        
+
     render(){
 
         return(
 
-
-            <Form>
+            <Form onSubmit={this.onSubmit}>
             <Form.Field>
                 <label> Amount to Contribute this time: </label>
                 <Input label="ether" labelPosition="right"
+                value={this.state.value}
+                onChange={event => this.setState({value: event.target.value})}
                 />
             </Form.Field>
             <Button primary>
