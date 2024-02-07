61cc6fee0b19f9818ac2699e4634e39e74f0d4c6
FrankLi123 <zoli@ucsd.edu>
2024-02-06 19:15:02 -0500

    retrieve each request and store into data structures

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -4,13 +4,28 @@ import React, { Component } from 'react';
 import Layout from '../../../components/Layout';
 import {Link} from '../../../routes';
 import { Card, Button} from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+
 
 class RequestIndex extends Component {
 
     static async getInitialProps(props){
         const {address} = props.query;
-        
-        return {address};
+        const campaign = Campaign(address);
+
+        // get the count number of requests
+        const requestCount = await campaign.methods.getRequestsCount().call();
+
+        // get the request array as a struct using JS
+        const requests = await Promise.all(
+            Array(requestCount).fill().map((element, index) =>{
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        console.log(requests);
+
+        return {address, requests};
     }
     render(){
         return(
