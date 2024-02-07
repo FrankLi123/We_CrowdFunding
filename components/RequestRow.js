c203082e72a3168392cd2b19a7cb245ddaac000a
FrankLi123 <zoli@ucsd.edu>
2024-02-06 19:46:30 -0500

    display request valus in each row

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
+import { Table } from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
 
 
 class RequestRow extends Component {
 
     render(){
-        return<div> @@@ </div>
+
+        const {Row, Cell} = Table;
+        // const {id, request} = this.props;
+        return(
+
+            <Row>
+
+                <Cell>{this.props.id}</Cell>
+                <Cell>{this.props.request.description}</Cell>
+                <Cell>{web3.utils.fromWei(this.props.request.value, 'ether')}</Cell>
+                <Cell>{this.props.request.recipient}</Cell>
+            </Row>
+        )
     }
 }
 
